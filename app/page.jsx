a53de0903750a5d6f72e2e435fcd04f32b8f997f
IsmaeilSaberi <ismaeilsaberi@gmail.com>
2023-06-05 08:42:26 -0700

    I completed the duration bar and its buttons and controllers!

--- app/page.jsx
+++ app/page.jsx
@@ -5,6 +5,7 @@ export default function Home() {
     <main className="flex flex-col gap-20 p-8">
       <VideoPlayer
         src={`https://agitated-nightingale-nintmjxec.storage.iran.liara.space/20230428_175834.mp4?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=342080d4-c2d1-45a3-a670-00ed1438736c%2F20230605%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20230605T070948Z&X-Amz-Expires=604800&X-Amz-SignedHeaders=host&X-Amz-Signature=13fd852506ead5905a68e7572f8bce6d887007f70f46ac3149c6dfe5c01185aa`}
+        // src={`/Trip1401.1.11.mp4`}
       />
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut hic vitae
--- components/video-player/index.jsx
+++ components/video-player/index.jsx
@@ -50,12 +50,61 @@ const VideoPlayer = ({ src }) => {
     }
   };
 
+  // DURATION
+  const [nowTime, setNowTime] = useState(0);
+  const [fullTime, setFullTime] = useState(821.5);
+  const videoTimeRef = useRef();
+
+  const timeBeauty = (inp) => {
+    let minutes = Math.floor(inp / 60);
+    let seconds = Math.floor(inp % 60);
+
+    if (minutes < 10) {
+      minutes = `0${minutes}`;
+    }
+    if (seconds < 10) {
+      seconds = `0${seconds}`;
+    }
+    let output = `${minutes}:${seconds}`;
+    return output;
+  };
+
+  setInterval(() => {
+    if (videoRef.current) {
+      if (videoRef.current.currentTime != null) {
+        setNowTime(videoRef.current.currentTime);
+        videoTimeRef.current.value = videoRef.current.currentTime;
+        setFullTime(videoRef.current.duration);
+      }
+    }
+  }, 1000);
+
   return (
     <div className="flex justify-center items-center">
       <div ref={fullscreenRef} className="relative w-[95%] max-w-[1000px]">
         <div className="ctrls absolute right-2 left-2 bottom-1 z-30">
           <div className="bg-transparent text-white p-3 rounded-md flex flex-col gap-3">
-            <div className="duration-bar w-full bg-white ">duration bar</div>
+            <div className="duration-bar w-full flex justify-between items-center gap-2">
+              <div className="w-10">{timeBeauty(nowTime)}</div>
+              <div className="w-full">
+                <input
+                  onChange={(e) => {
+                    setNowTime(e.target.value);
+                    videoTimeRef.current.value = e.target.value;
+                    videoRef.current.currentTime = e.target.value;
+                    // playingHandler(0);
+                  }}
+                  ref={videoTimeRef}
+                  type="range"
+                  step={"any"}
+                  min={0}
+                  max={fullTime}
+                  defaultValue={0}
+                  className="video-player-input h-2 cursor-pointer appearance-none rounded-lg border-transparent transparent w-full"
+                />
+              </div>
+              <div className="w-10">{timeBeauty(fullTime)}</div>
+            </div>
             <div className="flex justify-between items-center">
               <div className="flex gap-2">
                 <button className="text-2xl">
@@ -136,6 +185,7 @@ const VideoPlayer = ({ src }) => {
           </div>
         </div>
         <video
+          loop
           ref={videoRef}
           src={src}
           className="rounded-lg w-full z-20 h-full"
