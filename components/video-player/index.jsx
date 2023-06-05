d0d6a5f17b1896882590751e45cc6d800212f0ca
IsmaeilSaberi <ismaeilsaberi@gmail.com>
2023-06-05 02:24:06 -0700

    I made play and pause buttons functions and now they're working properly!

--- components/video-player/index.jsx
+++ components/video-player/index.jsx
@@ -1,22 +1,43 @@
+"use client";
 import { IoPlay, IoPause } from "react-icons/io5";
 import { FiVolume2, FiVolumeX } from "react-icons/fi";
 import { CgMiniPlayer } from "react-icons/cg";
 import { BiFullscreen } from "react-icons/bi";
+import { useState, useEffect, useRef } from "react";
 
 const VideoPlayer = ({ src }) => {
+  const videoRef = useRef();
+  const [isPlaying, setIsPlaying] = useState(0);
+
+  const playingHandler = (inp) => {
+    inp == 1 ? videoRef.current.play() : videoRef.current.pause();
+    setIsPlaying(inp);
+  };
+
+  // PLAY AND PAUSE
+
   return (
     <div className="flex justify-center items-center">
       <div className="relative w-[95%] max-w-[1000px]">
-        <div className="ctrls absolute right-2 left-2 bottom-1">
+        <div className="ctrls absolute right-2 left-2 bottom-1 z-30">
           <div className="bg-transparent text-white p-3 rounded-md flex flex-col gap-3">
             <div className="duration-bar w-full bg-white ">duration bar</div>
             <div className="flex justify-between items-center">
               <div className="flex gap-2">
                 <button className="text-2xl">
-                  <IoPlay />
-                </button>
-                <button className="text-2xl">
-                  <IoPause />
+                  {isPlaying == 0 ? (
+                    <IoPlay
+                      onClick={() => {
+                        playingHandler(1);
+                      }}
+                    />
+                  ) : (
+                    <IoPause
+                      onClick={() => {
+                        playingHandler(0);
+                      }}
+                    />
+                  )}
                 </button>
                 <button className="text-2xl">
                   <FiVolume2 />
@@ -44,7 +65,7 @@ const VideoPlayer = ({ src }) => {
             </div>
           </div>
         </div>
-        <video src={src} className="rounded-lg w-full" />
+        <video ref={videoRef} src={src} className="rounded-lg w-full z-20" />
       </div>
     </div>
   );
